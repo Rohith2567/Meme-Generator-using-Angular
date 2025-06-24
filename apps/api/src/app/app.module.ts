2a2783c2a583221ff16036d8936f55b4e08ae951
Rohith2567 <rohith1asbr@gmail.com>
2025-06-25 03:35:14 +0530

    build(production): added a script for production deployment

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { Module } from '@nestjs/common';
 import { MemeModule } from './meme/meme.module';
-import { ThrottlerGuard, ThrottlerModule} from '@nestjs/throttler';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [MemeModule, ThrottlerModule.forRoot({
-      throttlers: [
-        {
-          ttl: 300000,
-          limit: 3,
-        },
-      ],
-    })],
+    throttlers: [
+      {
+        ttl: 300000,
+        limit: 3,
+      },
+    ],
+  }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'sigma-memer/browser'), // sigma-memer\dist\apps\sigma-memer\browser\index.html
+      // exclude: ['/api*'], 
+      serveStaticOptions: {
+        index: false, // Disable automatic index.html
+      }
+    }),
+  ],
   controllers: [],
   providers: [{
     provide: APP_GUARD,
